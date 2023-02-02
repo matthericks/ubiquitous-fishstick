dcf8fce19fecf0ae114728871a93c4b8f9c2cc02
Matt Hericks <matt.hericks@westshorehome.com>
2023-02-02 14:34:34 -0600

    bug reproduction commit

---
+++ src/MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage {
+    public static readonly BindableProperty Visible1Property = BindableProperty.Create(nameof(Visible1), typeof(bool), typeof(MainPage));
+    public static readonly BindableProperty Visible2Property = BindableProperty.Create(nameof(Visible2), typeof(bool), typeof(MainPage));
+
+    public bool Visible1 { get => (bool)GetValue(Visible1Property); set => SetValue(Visible1Property, value); }
+    public bool Visible2 { get => (bool)GetValue(Visible2Property); set => SetValue(Visible2Property, value); }
+
+    public List<int> Items1 { get; } = new() { 1, 2, 3 };
+    public List<int> Items2 { get; } = new() { 4, 5, 6 };
+    
+    public MainPage() => InitializeComponent();
+
+    private void Button1Clicked(object sender, EventArgs e) => Visible1 = !Visible1;
+
+    private void Button2Clicked(object sender, EventArgs e) => Visible2 = !Visible2;
+}
